e42288202727c0db08fb9d4aa319eacfabe28b84
Alex Grintsvayg <alex@grin.io>
2015-09-06 13:52:05 -0400

    some html, toggle enabled via site

--- webserver/webserver.go
+++ webserver/webserver.go
@@ -1,10 +1,16 @@
 package webserver
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"html/template"
+	"log"
 	"net"
 	"net/http"
+	"os"
+	"path/filepath"
+	"runtime"
 	"strconv"
 	"strings"
 
@@ -15,16 +21,44 @@ import (
 	"github.com/topscore/sup/Godeps/_workspace/src/github.com/zenazn/goji/web"
 )
 
-func homeRoute(c web.C, w http.ResponseWriter, r *http.Request) {
-	status := common.GetStatus()
-	if status.Disabled {
-		fmt.Fprintln(w, "<h1>sup is disabled</h1>")
-	} else {
-		fmt.Fprintln(w, "<h1>sup is running</h1>")
+var templates *template.Template
+
+func loadTemplates() error {
+	var tmpl []string
+
+	_, filename, _, _ := runtime.Caller(1)
+
+	fn := func(path string, f os.FileInfo, err error) error {
+		if f.IsDir() != true && strings.HasSuffix(f.Name(), ".html") {
+			tmpl = append(tmpl, path)
+		}
+		return nil
 	}
 
-	fmt.Fprintln(w, "<h3>last ping at "+status.LastRunAt+"</h3>")
-	fmt.Fprintln(w, "<h3>last status "+strconv.Itoa(status.LastStatus)+"</h3>")
+	err := filepath.Walk(filepath.Dir(filename)+"/templates", fn)
+
+	if err != nil {
+		return err
+	}
+
+	templates = template.Must(template.ParseFiles(tmpl...))
+	return nil
+}
+
+func getTemplate(name string, data interface{}) string {
+	var doc bytes.Buffer
+	templates.ExecuteTemplate(&doc, name, data)
+	return doc.String()
+}
+
+func homeRoute(c web.C, w http.ResponseWriter, r *http.Request) {
+	status := common.GetStatus()
+	templateArgs := map[string]interface{}{
+		"enabled":      !status.Disabled,
+		"lastPingTime": status.LastRunAt,
+		"lastStatus":   strconv.Itoa(status.LastStatus),
+	}
+	fmt.Fprintln(w, getTemplate("home", templateArgs))
 }
 
 func statusRoute(c web.C, w http.ResponseWriter, r *http.Request) {
@@ -37,7 +71,20 @@ func robotsRoute(c web.C, w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "User-agent: *\nDisallow: /")
 }
 
+func toggleEnabledRoute(c web.C, w http.ResponseWriter, r *http.Request) {
+	status := common.GetStatus()
+	status.Disabled = !status.Disabled
+	log.Printf("setting disabled to %t\n", status.Disabled)
+	common.SetStatus(status)
+	http.Redirect(w, r, "/", http.StatusFound)
+}
+
 func StartWebServer(bind, auth string) error {
+	err := loadTemplates()
+	if err != nil {
+		return err
+	}
+
 	if auth != "" {
 		authParts := strings.Split(auth, ":")
 		goji.Use(httpauth.SimpleBasicAuth(authParts[0], authParts[1]))
@@ -46,6 +93,7 @@ func StartWebServer(bind, auth string) error {
 	goji.Get("/", homeRoute)
 	goji.Get("/status", statusRoute)
 	goji.Get("/robots.txt", robotsRoute)
+	goji.Get("/toggleEnabled", toggleEnabledRoute)
 
 	listener, err := net.Listen("tcp", bind)
 	if err != nil {
